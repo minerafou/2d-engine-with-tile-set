2ef8d2a4cb0f6e1806b9786bbf31e2fcbf2c6426
minerafou <110623516+minerafou@users.noreply.github.com>
2022-09-10 19:51:19 +0200

    update level to try cam y

--- walls.py
+++ walls.py
@@ -3,10 +3,10 @@ import pygame
 def SetWalls():
     #set level walls
     walls = []
-    walls_x =      [    0,  300,  375,  450,  525,  600,    0]
-    walls_y =      [  -75, -150, -300, -150, -225, -150, -500]
-    walls_width =  [10000,   75,   75,   75,   75,   75,   75]
-    walls_height = [   75,   75,   75,   75,  150,   75,  450]
+    walls_x =      [    0,  300,  375,  450,  525,  600,    0,  225,   75]
+    walls_y =      [  -75, -150, -300, -150, -225, -150, -525, -375, -450]
+    walls_width =  [10000,   75,   75,   75,   75,   75,   75,   75,   75]
+    walls_height = [   75,   75,   75,   75,  150,   75,  450,   75,   75]
     for i in range(len(walls_x)):
         walls.append(Walls(walls_x[i], walls_y[i], walls_width[i], walls_height[i]))
     return walls
